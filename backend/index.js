42afdad203c6ebd305e5c169c1ba5269ee9f300e
akshayp7611 <akshaypatil7611@gmail.com>
2025-09-25 10:33:58 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://job-portal-frontend-three-zeta.vercel.app/", credentials: true }));
 
 // Lazy DB connection middleware
 let isConnected = false;
