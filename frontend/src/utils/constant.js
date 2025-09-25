bde1f916bfdaefce1007189bc1ff410f8364f339
akshayp7611 <akshaypatil7611@gmail.com>
2025-09-25 10:39:18 +0530

    env changes in frontend

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-       const  VITE_USER_API_END_POINT="http://localhost:8000/api/v1/user";
+       const  VITE_USER_API_END_POINT="https://job-portal-seven-ruddy.vercel.app/api/v1/user";
 export default VITE_USER_API_END_POINT;
 
-export const JOB_API_END_POINT= "http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT= "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT= "http://localhost:8000/api/v1/company";
+export const JOB_API_END_POINT= "https://job-portal-seven-ruddy.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT= "https://job-portal-seven-ruddy.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT= "https://job-portal-seven-ruddy.vercel.app/api/v1/company";
