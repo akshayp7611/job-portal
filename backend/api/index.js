22990746aabbf17b340f04cfa396edc09e007a13
akshayp7611 <akshaypatil7611@gmail.com>
2025-09-25 10:48:32 +0530

    api change

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,11 +3,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import serverless from "serverless-http";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
+import connectDB from "../utils/db.js";
+import userRoute from "../routes/user.route.js";
+import companyRoute from "../routes/company.route.js";
+import jobRoute from "../routes/job.route.js";
+import applicationRoute from "../routes/application.route.js";
 
 dotenv.config();
 
