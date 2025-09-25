3550e9889dddbc5b8ba972e293bdf1d8f0c9d923
akshayp7611 <akshaypatil7611@gmail.com>
2025-09-25 10:51:00 +0530

    litsen added

--- backend/api/index.js
+++ backend/api/index.js
@@ -47,4 +47,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to Job Portal API");
 });
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 export default serverless(app);
